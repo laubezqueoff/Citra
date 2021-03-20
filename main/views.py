08ee7aa84dd6f00be5dd0ea05170c0f15d615bd0
Antonio Jesús García Nieto <curiosidadcompacta@gmail.com>
2021-03-20 15:59:15 +0100

    [Feat] Añadida la lógica de login, logout y métodos de manejo de variables de sesión

--- main/views.py
+++ main/views.py
@@ -1,3 +1,120 @@
+from main.models import Person, CustomUser, CustomAdmin, Owner, ShopType, ProductType, Shop, Product, SubscriptionType, PromotionType, Subscription, Promotion, Booking, Review, Chat, ChatMessage, Thread, ForumMessage
 from django.shortcuts import render
+import requests
 
-# Create your views here.
+
+def login(request):
+    ''' Logea una persona en la aplicación.\n
+        POST    -> Lleva la inicio con el contexto actualizado \n
+        GET     -> Lleva al formulario de login
+    '''
+    if request.method == 'POST': #Si es un POST redirijimos a la vista de index con el context actualizado
+        try:
+            username = request.POST['name']             #Sacamos el valor de la propiedad 'name' del formulario
+            password = request.POST['password']         #Sacamos el valor de la propiedad 'password' del formulario
+
+            person = Person.objects.get(username=username,password=password)        #Buscamos el usuario por su contraseña y nombre
+            rol_and_id = whoIsWho(person)
+            
+            update_context(person.id,rol_and_id[0],rol_and_id[1],True)
+
+        except Exception as e:
+            print(e)
+    
+    else: #Si es un GET redirijimos a la vista de login
+        return render(request, 'login.html')
+
+def logout(request):
+    ''' Deslogea una persona en la aplicación.\n
+        POST    -> None \n
+        GET     -> Lleva al formulario de login
+    '''
+    if request.method == 'GET': #Si es un GET redirijimos a la vista de index con el context actualizado
+        try:
+            
+           person_id,rol,rol_id,is_active = get_context()
+
+           update_context(person_id,rol,rol_id,False)
+
+        except Exception as e:
+            print(e)
+        
+        return render(request, 'index.html')
+        
+def whoIsWho(person):
+    ''' Identifica el rol e id de una persona\n
+        In: Person\n
+        Out: List[rol,rol_id]
+    '''
+    res = []
+    try:
+        cu = CustomUser.objects.get(person=person).id
+        continue_searching = False
+        res=["User",cu]
+    except:
+        continue_searching = True
+
+    if continue_searching:
+        try:
+            ca = CustomAdmin.objects.get(person=person).id
+            res=["Admin",ca]
+        except:
+            continue_searching = True
+    
+    if continue_searching:
+        try:
+            o = Owner.objects.get(person=person).id
+            res=["Owner",o]
+        except:
+            continue_searching = True
+    
+    return res
+
+def update_context(person_id,rol,rol_id,is_active):
+    ''' Actualiza el context en función de los parámetros de entrada\n
+        In: person_id (id de la persona), rol (String, puede ser Owner,Admin o User),rol_id (id de la persona en su rol), is_active (se encuentra usando la web)\n
+        Out: Bool. True si todo va bien, False si algo falla
+    '''
+    res = True
+    try:
+        request.session['person_id'] = person_id
+        request.session['rol'] = rol
+        request.session['person_id'] = rol_id
+        request.session['is_active'] = is_active
+    except:
+        res = False
+    
+    return res
+
+def delete_context():
+    ''' Elimina el context\n
+        In: None \n
+        Out: Bool. True si todo va bien, False si algo falla
+    '''
+
+    res = True
+    try:
+        del request.session['person_id']
+        del request.session['rol']
+        del request.session['person_id']
+        del request.session['is_active']
+
+    except:
+        res = False
+
+    return res
+
+def get_context():
+    ''' Actualiza el context en función de los parámetros de entrada\n
+        In: None \n
+        Out: person_id (id de la persona), rol (String, puede ser Owner,Admin o User),rol_id (id de la persona en su rol), is_active (se encuentra usando la web)
+    '''
+
+    person_id   =   request.session['person_id'] 
+    rol         =   request.session['rol']
+    rol_id      =   request.session['person_id']
+    is_active   =   request.session['is_active']
+
+    return person_id,rol,rol_id,is_active
+
+
