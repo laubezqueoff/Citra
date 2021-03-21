f30f4f65c09df09eb9f981c57e47584f4a7f6ead
Enrique Jordá <enrjorarr@alum.us.es>
2021-03-21 12:42:10 +0100

    Template v1 done

--- main/views.py
+++ main/views.py
@@ -1,3 +1,10 @@
 from django.shortcuts import render
 
 # Create your views here.
+
+
+def home(request):
+    return render(request,'main/home.html', {})
+
+def login(request):
+    return render(request,'main/login.html', {})
