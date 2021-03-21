5e90106350e9e86f509e10d06e03610b0cfd963d
pepe ruiz <josruialb@alum.us.es>
2021-03-21 12:30:31 +0100

    [Feat] Se añade el listado de tiendas con su show correspondiente
    Creada la funcionalidad de listar tiendas y mostrar sus detalles.

--- main/views.py
+++ main/views.py
@@ -1,6 +1,7 @@
 from main.models import Person, CustomUser, CustomAdmin, Owner, ShopType, ProductType, Shop, Product, SubscriptionType, PromotionType, Subscription, Promotion, Booking, Review, Chat, ChatMessage, Thread, ForumMessage
-from django.shortcuts import render
-import requests
+from django.shortcuts import render, redirect, get_object_or_404
+#import requests
+import urllib.request
 
 
 def login(request):
@@ -8,46 +9,52 @@ def login(request):
         POST    -> Lleva la inicio con el contexto actualizado \n
         GET     -> Lleva al formulario de login
     '''
-    if request.method == 'POST': #Si es un POST redirijimos a la vista de index con el context actualizado
+    if request.method == 'POST':  # Si es un POST redirijimos a la vista de index con el context actualizado
         try:
-            username = request.POST['username']             #Sacamos el valor de la propiedad 'name' del formulario
-            password = request.POST['password']         #Sacamos el valor de la propiedad 'password' del formulario
+            # Sacamos el valor de la propiedad 'name' del formulario
+            username = request.POST['username']
+            # Sacamos el valor de la propiedad 'password' del formulario
+            password = request.POST['password']
 
-            person = Person.objects.get(username=username,password=password)        #Buscamos el usuario por su contraseña y nombre
+            # Buscamos el usuario por su contraseña y nombre
+            person = Person.objects.get(username=username, password=password)
             rol_and_id = whoIsWho(person)
-            
-            update_context(request,person.id,rol_and_id[0],rol_and_id[1],True)
+
+            update_context(request, person.id,
+                           rol_and_id[0], rol_and_id[1], True)
 
         except Exception as e:
             print(e)
-        
-        person_id,rol,rol_id,is_active= get_context(request)
-        context = [person_id,rol,rol_id,is_active]
-        
+
+        person_id, rol, rol_id, is_active = get_context(request)
+        context = [person_id, rol, rol_id, is_active]
+
         # Es importante pasar el context en todas las vistas.
         # Cambiar index.html por tu vista en tu método
 
-        return render(request, 'promote.html',{"context" : context})
-    else: #Si es un GET redirijimos a la vista de login
+        return render(request, 'promote.html', {"context": context})
+    else:  # Si es un GET redirijimos a la vista de login
         return render(request, 'login.html')
+
 
 def logout(request):
     ''' Deslogea una persona en la aplicación.\n
         POST    -> None \n
         GET     -> Lleva al formulario de login
     '''
-    if request.method == 'GET': #Si es un GET redirijimos a la vista de index con el context actualizado
+    if request.method == 'GET':  # Si es un GET redirijimos a la vista de index con el context actualizado
         try:
-            
-           person_id,rol,rol_id,is_active = get_context()
 
-           update_context(person_id,rol,rol_id,False)
+            person_id, rol, rol_id, is_active = get_context()
+
+            update_context(person_id, rol, rol_id, False)
 
         except Exception as e:
             print(e)
-        
+
         return render(request, 'index.html')
-        
+
+
 def whoIsWho(person):
     ''' Identifica el rol e id de una persona\n
         In: Person\n
@@ -57,7 +64,7 @@ def whoIsWho(person):
     try:
         cu = CustomUser.objects.get(person=person).id
         continue_searching = False
-        res=["User",cu]
+        res = ["User", cu]
     except:
         continue_searching = True
     print("==========================================================")
@@ -65,20 +72,21 @@ def whoIsWho(person):
     if continue_searching:
         try:
             ca = CustomAdmin.objects.get(person=person).id
-            res=["Admin",ca]
+            res = ["Admin", ca]
         except:
             continue_searching = True
-    
+
     if continue_searching:
         try:
             o = Owner.objects.get(person=person).id
-            res=["Owner",o]
+            res = ["Owner", o]
         except:
             continue_searching = True
-    
+
     return res
 
-def update_context(request,person_id,rol,rol_id,is_active):
+
+def update_context(request, person_id, rol, rol_id, is_active):
     ''' Actualiza el context en función de los parámetros de entrada\n
         In: person_id (id de la persona), rol (String, puede ser Owner,Admin o User),rol_id (id de la persona en su rol), is_active (se encuentra usando la web)\n
         Out: Bool. True si todo va bien, False si algo falla
@@ -88,7 +96,7 @@ def update_context(request,person_id,rol,rol_id,is_active):
     print(rol)
     print(rol_id)
     print(is_active)
-    
+
     try:
         request.session['person_id'] = str(person_id)
         request.session['rol'] = str(rol)
@@ -96,10 +104,11 @@ def update_context(request,person_id,rol,rol_id,is_active):
         request.session['is_active'] = str(is_active)
     except:
         res = False
-    
+
     print(res)
 
     return res
+
 
 def delete_context():
     ''' Elimina el context\n
@@ -119,27 +128,39 @@ def delete_context():
 
     return res
 
+
 def get_context(request):
     ''' Actualiza el context en función de los parámetros de entrada\n
         In: None \n
         Out: person_id (id de la persona), rol (String, puede ser Owner,Admin o User),rol_id (id de la persona en su rol), is_active (se encuentra usando la web)
     '''
-    person_id   =   request.session['person_id'] 
-    rol         =   request.session['rol']
-    rol_id      =   request.session['rol_id']
-    is_active   =   request.session['is_active']
+    person_id = request.session['person_id']
+    rol = request.session['rol']
+    rol_id = request.session['rol_id']
+    is_active = request.session['is_active']
 
-    return person_id,rol,rol_id,is_active
+    return person_id, rol, rol_id, is_active
 
 
 def promotion_product(request):
     if request.method == 'GET':
         return render(request, 'promote.html')
     product = Product.objects.get(id=1)
-    person_id,rol,rol_id,is_active = get_context(request)
+    person_id, rol, rol_id, is_active = get_context(request)
     print(person_id)
     promotion = Promotion.obj
-    person = Person.objects.get(username=username,password=password)        #Buscamos el usuario por su contraseña y nombre
+    # Buscamos el usuario por su contraseña y nombre
+    person = Person.objects.get(username=username, password=password)
     rol_and_id = whoIsWho(person)
-    
-    update_context(person.id,rol_and_id[0],rol_and_id[1],True)
+
+    update_context(person.id, rol_and_id[0], rol_and_id[1], True)
+
+
+def list_shop(request):
+    shops = Shop.objects.all()
+    return render(request, 'shops.html', {'shops': shops})
+
+
+def list_shop_details(request, id_shop):
+    shop = get_object_or_404(Shop, pk=id_shop)
+    return render(request, 'shopDetail.html', {'shop': shop})
