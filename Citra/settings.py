21b2a79d5bfb14708627dbf9e482426b224a1d29
Antonio Jesús García Nieto <curiosidadcompacta@gmail.com>
2021-03-26 17:31:20 +0100

    [fix] Cambio de ficheros de configuración de travis y heroku
    Co-Authored-By: laubezqueoff <56026543+laubezqueoff@users.noreply.github.com>
    Co-Authored-By: antcorlav <56026662+antcorlav@users.noreply.github.com>

--- Citra/settings.py
+++ Citra/settings.py
@@ -9,6 +9,27 @@ https://docs.djangoproject.com/en/3.1/ref/settings/
 
 from pathlib import Path
 import os
+import django_heroku
+
+
+
+BASEURL = 'https://citra-ispp.herokuapp.com/'
+
+APIS = {
+    'authentication': BASEURL,
+    'base': BASEURL,
+    'booth': BASEURL,
+    'census': BASEURL,
+    'mixnet': BASEURL,
+    'postproc': BASEURL,
+    'store': BASEURL,
+    'visualizer': BASEURL,
+    'voting': BASEURL,
+}
+
+
+
+django_heroku.settings(locals())
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
--- Procfile
+++ Procfile
@@ -1,1 +1,2 @@
+release: sh -c 'cd Citra && python manage.py migrate'
 web: sh -c 'cd doc && cd Citra && gunicorn citra.wsgi --log-file -'
