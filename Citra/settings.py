8e00087ac0e9c5a8e962ce30b7b8c2fd2a1ec7d5
VicenteLo98 <vlopezvazquez3@gmail.com>
2021-04-16 17:36:40 +0200

    [fix] Arreglos imagenes
    Añadido url en las vistas y se ha quitado "media" del settings

--- Citra/settings.py
+++ Citra/settings.py
@@ -148,9 +148,9 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 
-MEDIA_URL = '/media/'
+# MEDIA_URL = '/media/'
 
-MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+# MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
 STATIC_ROOT = os.path.join(os.path.dirname(os.path.dirname(__file__)), 'staticfiles')
 
