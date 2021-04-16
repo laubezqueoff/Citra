b12c15f0559f3a8ebd37cc6b0b98de82d66c16b7
VicenteLo98 <vlopezvazquez3@gmail.com>
2021-04-16 18:47:08 +0200

    [fix] Añadido archivo backends.py

--- Citra/settings.py
+++ Citra/settings.py
@@ -205,8 +205,9 @@ TEMPLATE_DIRS = (
 AWS_ACCESS_KEY_ID = os.environ.get('AWS_ACCESS_KEY_ID', '')
 AWS_SECRET_ACCESS_KEY = os.environ.get('AWS_SECRET_ACCESS_KEY', '')
 AWS_STORAGE_BUCKET_NAME = os.environ.get('AWS_STORAGE_BUCKET_NAME', '')
+AWS_S3_REGION_NAME = os.environ.get('AWS_S3_REGION_NAME', '')
 
 AWS_S3_FILE_OVERWRITE = False
 AWS_DEFAULT_ACL = None
 
-DEFAULT_FILE_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
+DEFAULT_FILE_STORAGE = 'main.backends.MediaStorageBackend'
